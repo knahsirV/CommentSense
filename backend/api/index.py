8010457c292c2095bea7512a5888f4ecbd61d5ca
Vrishank Viswanath <vrishank@utexas.edu>
2023-05-25 14:28:16 -0500

    seeing if route is the issue

--- backend/api/index.py
+++ backend/api/index.py
@@ -11,20 +11,20 @@ def home():
     return "Hello, Flask!"
 
 
-@app.route("/sentiments/<video_id>")
-def sentiments(video_id):
-    video_id = str(video_id)
-    sentiments = comment_tools.check_if_analyzed(video_id)
-    if len(sentiments.data) > 0:
-        year_analyzed = int(sentiments.data[0]["created_at"][0:4])
-        year_now = int(datetime.now().year)
-        if year_now - year_analyzed == 0:
-            return sentiments.data[0]["sentiments"]
-    comments = comment_tools.get_video_comments(video_id)
-    # sentiments = comment_tools.analyze_comments(comments)
-    # comment_tools.add_to_analyzed(video_id, sentiments)
-    # return sentiments
-    return "Not analyzed"
+# @app.route("/sentiments/<video_id>")
+# def sentiments(video_id):
+#     video_id = str(video_id)
+#     sentiments = comment_tools.check_if_analyzed(video_id)
+#     if len(sentiments.data) > 0:
+#         year_analyzed = int(sentiments.data[0]["created_at"][0:4])
+#         year_now = int(datetime.now().year)
+#         if year_now - year_analyzed == 0:
+#             return sentiments.data[0]["sentiments"]
+#     comments = comment_tools.get_video_comments(video_id)
+#     # sentiments = comment_tools.analyze_comments(comments)
+#     # comment_tools.add_to_analyzed(video_id, sentiments)
+#     # return sentiments
+#     return "Not analyzed"
 
 
 if __name__ == "__main__":
