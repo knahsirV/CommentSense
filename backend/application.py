ef5e267f0e5f09b0392d58eb8605cb744d6a726e
Vrishank Viswanath <vrishank@utexas.edu>
2023-05-26 16:54:08 -0500

    added more error handling just in case

--- backend/application.py
+++ backend/application.py
@@ -13,18 +13,21 @@ def home():
 
 @application.route("/sentiments/<video_id>")
 def sentiments(video_id):
-    video_id = str(video_id)
-    sentiments = comment_tools.check_if_analyzed(video_id)
-    if len(sentiments.data) > 0:
-        year_analyzed = int(sentiments.data[0]["created_at"][0:4])
-        year_now = int(datetime.now().year)
-        if year_now - year_analyzed == 0:
-            return sentiments.data[0]["sentiments"]
-    comments = comment_tools.get_video_comments(video_id)
-    sentiments = comment_tools.analyze_comments(comments)
-    if (not sentiments is dict) and (not sentiments.get("error")):
-        comment_tools.add_to_analyzed(video_id, sentiments)
-    return sentiments
+    try:
+        video_id = str(video_id)
+        sentiments = comment_tools.check_if_analyzed(video_id)
+        if len(sentiments.data) > 0:
+            year_analyzed = int(sentiments.data[0]["created_at"][0:4])
+            year_now = int(datetime.now().year)
+            if year_now - year_analyzed == 0:
+                return sentiments.data[0]["sentiments"]
+        comments = comment_tools.get_video_comments(video_id)
+        sentiments = comment_tools.analyze_comments(comments)
+        if (not sentiments is dict) and (not sentiments.get("error")):
+            comment_tools.add_to_analyzed(video_id, sentiments)
+        return sentiments
+    except Exception as e:
+        return {"error": str(e)}
 
 
 if __name__ == "__main__":
