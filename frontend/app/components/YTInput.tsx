439cf039db137603e1e97f0e2afe17c15a688260
Vrishank Viswanath <vrishank@utexas.edu>
2023-06-15 10:37:10 -0500

    added loading spinner to video input bar

--- frontend/app/components/YTInput.tsx
+++ frontend/app/components/YTInput.tsx
@@ -2,6 +2,7 @@
 import { useForm, SubmitHandler } from "react-hook-form";
 import { PlayIcon, ArrowRightIcon } from "@heroicons/react/20/solid";
 import { useRouter } from "next/navigation";
+import { use, useState } from "react";
 
 export interface IFormInput {
   url: string;
@@ -15,9 +16,12 @@ const YTInput = ({ onDashboard }: { onDashboard?: boolean }) => {
     formState: { errors, isDirty, isValid },
   } = useForm<IFormInput>({ mode: "onBlur" });
 
+  const [loading, setLoading] = useState(false);
+
   const onSubmit: SubmitHandler<IFormInput> = async (data) => {
     const vidId = data.url.split("v=")[1];
     router.push(`/sense/${vidId}`);
+    setLoading(true);
   };
 
   return (
@@ -48,7 +52,20 @@ const YTInput = ({ onDashboard }: { onDashboard?: boolean }) => {
           disabled={!isDirty || !isValid}
           className="group h-full rounded-r-full px-4 transition-colors duration-200 hover:bg-red-500 disabled:cursor-not-allowed disabled:hover:bg-zinc-700"
         >
-          <ArrowRightIcon className="h-5 w-5 text-zinc-700 transition-colors duration-200 group-hover:text-white" />
+          {loading ? (
+            <svg className="h-5 w-5 animate-spin" viewBox="3 3 18 18">
+              <path
+                className="fill-zinc-800 group-hover:fill-red-800"
+                d="M12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12C19 8.13401 15.866 5 12 5ZM3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12Z"
+              ></path>
+              <path
+                className="fill-zinc-600 group-hover:fill-red-200"
+                d="M16.9497 7.05015C14.2161 4.31648 9.78392 4.31648 7.05025 7.05015C6.65973 7.44067 6.02656 7.44067 5.63604 7.05015C5.24551 6.65962 5.24551 6.02646 5.63604 5.63593C9.15076 2.12121 14.8492 2.12121 18.364 5.63593C18.7545 6.02646 18.7545 6.65962 18.364 7.05015C17.9734 7.44067 17.3403 7.44067 16.9497 7.05015Z"
+              ></path>
+            </svg>
+          ) : (
+            <ArrowRightIcon className="h-5 w-5 text-zinc-700 transition-colors duration-200 group-hover:text-white" />
+          )}
         </button>
       </form>
       <span
